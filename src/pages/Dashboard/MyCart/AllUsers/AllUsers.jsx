a60f2b71276f37f0a943f6ff137dbbb041bafdb2
Mahmudul107 <mahmudulislam378@gmail.com>
2023-05-31 16:09:39 +0600

    Made admin and remove admin completed

--- src/pages/Dashboard/MyCart/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/MyCart/AllUsers/AllUsers.jsx
@@ -9,18 +9,19 @@ const AllUsers = () => {
     return res.json();
   });
 
-  const handleMakeAdmin = id => {
-    fetch(`http://localhost:5000/users/admin/${id}`,{
+  const handleMakeAdmin = user => {
+    fetch(`http://localhost:5000/users/admin/${user._id}`,{
         method: 'PATCH'
     })
     .then( res => res.json())
     .then( data =>{
         console.log(data);
         if(data.modifiedCount){
+            refetch();
             Swal.fire({
                 position: 'top-end',
                 icon: 'success',
-                title: 'Your work has been saved',
+                title: `${user.name} is Admin now!`,
                 showConfirmButton: false,
                 timer: 1500
               })
@@ -29,7 +30,28 @@ const AllUsers = () => {
   }
 
   const handleDelete = user =>{
-
+    Swal.fire({
+        title: "Sure to remove an Admin?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: "DELETE"
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              if (data.deletedCount > 0) {
+                refetch();
+                Swal.fire("Deleted!", "Your file has been deleted.", "success");
+              }
+            });
+        }
+      });
   }
 
   return (
@@ -56,7 +78,7 @@ const AllUsers = () => {
                     <td>{user.name}</td>
                     <td>{user.email}</td>
                     <td>{user.role === 'admin' ? 'admin' : <button
-                      onClick={() => handleMakeAdmin(user._id)}
+                      onClick={() => handleMakeAdmin(user)}
                       className="btn btn-ghost bg-orange-600 text-white"
                     >
                       <FaUserShield/>
